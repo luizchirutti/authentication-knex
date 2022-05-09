126ee50818c8cee3638e3ce1cde223723ad6bc5b
Luiz Chirutti <luizchirutti@gmail.com>
2022-05-09 16:50:16 -0300

    add migrations

---
+++ src/database/migrations/20211020043754_create_projects_table.js
@@ -0,0 +1,20 @@
+
+exports.up = knex => knex.schema.createTable('projects', table => {
+    table.increments('id')
+    table.text('title')
+
+    //relacionamento
+
+    table.integer(user_id)
+        .references('user.id')
+        .notNullable()
+        .onDelete('CASCADE')
+
+    table.timestamps(true, true)
+  })
+
+
+exports.down = knex => knex.schema.dropTable('users')
+
+
+
